6f91c8df9fa12a38834ff2100c6ba6e1d631a52f
tonni-alter-dev <tanniakhter123@gmail.com>
2026-09-18 20:22:28 +0600

    SDFSD

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ app.use((err, req, res, next) => {
 // ============================================================
 // Server start
 // ============================================================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
